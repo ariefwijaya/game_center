fd330c46e1b5bff526041a70cd0885d7be7a01fc
Arief Wijaya <ariefwiijaya@gmail.com>
2022-10-24 16:11:26 +0700

    feat: add splash screem

--- lib/modules/splash/screens/splash_screen.dart
+++ lib/modules/splash/screens/splash_screen.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/material.dart';
+import 'package:game_center/main.import.dart';
+import 'package:game_center/main.library.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    //We simulate the splashscreen only for UX purpose
+    //We can use real logic such as authentication or assets preparation while showing splashscreen
+    Future.delayed(const Duration(seconds: 2))
+        .then((value) => context.replaceRoute(const OnboardingRoute()));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Assets.images.gameCenterLogo.image(height: 250)),
+          const SizedBox(height: 10),
+          const LinearProgressIndicator(),
+          const SizedBox(height: 10),
+          TypographyCustom.heading.h1(Env.appName)
+        ],
+      ),
+    );
   }
 }
